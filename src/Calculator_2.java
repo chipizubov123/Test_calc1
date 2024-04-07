66ece2cb39e9af7c60ffdeb601010b43556afffc
chipizubov123 <chipizubov123@mail.ru>
2024-04-08 00:57:25 +0500

    калькулятор 2 версии, конец

--- src/Calculator_2.java
+++ src/Calculator_2.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Calculator_2 {
@@ -34,6 +30,7 @@ public class Calculator_2 {
                 char target = CharArray[i];
                 if (target == '+' || target == '/' || target == '*' || target == '-') {
                     Operation = i;
+                    break;
                 }
             }
         } catch (Exception e) {
@@ -43,34 +40,30 @@ public class Calculator_2 {
     }
 
     static int Value(String s) {
-        try {
-            return switch (s) {
-                case "1" -> 1;
-                case "2" -> 2;
-                case "3" -> 3;
-                case "4" -> 4;
-                case "5" -> 5;
-                case "6" -> 6;
-                case "7" -> 7;
-                case "8" -> 8;
-                case "9" -> 9;
-                case "10" -> 10;
-                case "I" -> 1;
-                case "II" -> 2;
-                case "III" -> 3;
-                case "IV" -> 4;
-                case "V" -> 5;
-                case "VI" -> 6;
-                case "VII" -> 7;
-                case "VIII" -> 8;
-                case "IX" -> 9;
-                case "X" -> 10;
-                default -> throw new Exception("Некорректно введено выражение. Выражение должно содержать только арабские или только римские цифры от 1 до 10 и обязательно один знак: 1 + 1 или I + V");
-            };
-        } catch (Exception e) {
-            System.out.println("Некорректно введено выражение. Выражение должно содержать только арабские или только римские цифры от 1 до 10 и обязательно один знак: 1 + 1 или I + V");
-        }
-        return 0;
+        return switch (s) {
+            case "1" -> 1;
+            case "2" -> 2;
+            case "3" -> 3;
+            case "4" -> 4;
+            case "5" -> 5;
+            case "6" -> 6;
+            case "7" -> 7;
+            case "8" -> 8;
+            case "9" -> 9;
+            case "10" -> 10;
+            case "I" -> 1;
+            case "II" -> 2;
+            case "III" -> 3;
+            case "IV" -> 4;
+            case "V" -> 5;
+            case "VI" -> 6;
+            case "VII" -> 7;
+            case "VIII" -> 8;
+            case "IX" -> 9;
+            case "X" -> 10;
+            default ->
+                    throw new RuntimeException("Некорректно введено выражение. Выражение должно содержать только арабские или только римские цифры от 1 до 10 и обязательно один знак: 1 + 1 или I + V");
+        };
     }
 
     static String Roman(int i) {
@@ -85,29 +78,27 @@ public class Calculator_2 {
     }
 
     static String Calculated() {
-        String [] ArabicNumber = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        String[] ArabicNumber = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
         String[] RomanNumber = {"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        int value1 = Value(s1.trim().replace(" ","").substring(0, Operation));
-        int value2 = Value(s1.trim().replace(" ","").substring(Operation + 1, CharArray.length));
-        try {
-            if (s1.trim().replace(" ","").substring(0, Operation).equals(RomanNumber[value1]) && s1.trim().replace(" ","").substring(Operation + 1, CharArray.length).equals(RomanNumber[value2])) {
-                return Roman(Result(value1, value2));
-            } else if (s1.trim().replace(" ","").substring(0, Operation).equals(ArabicNumber[value1])&& s1.trim().replace(" ","").substring(Operation + 1, CharArray.length).equals(ArabicNumber[value2])) {
-                return Integer.toString(Result(value1, value2));
-            }else {throw new Exception("Некорректно введено выражение. Выражение должно содержать только арабские или только римские цифры от 1 до 10 и обязательно один знак: 1 + 1 или I + V");}
-        } catch (Exception e) {
-            System.out.println("Некорректно введено выражение. Выражение должно содержать только арабские или только римские цифры от 1 до 10 и обязательно один знак: 1 + 1 или I + V");
+        int value1 = Value(s1.trim().replace(" ", "").substring(0, Operation));
+        int value2 = Value(s1.trim().replace(" ", "").substring(Operation + 1, CharArray.length));
+
+        if (s1.trim().replace(" ", "").substring(0, Operation).equals(RomanNumber[value1]) && s1.trim().replace(" ", "").substring(Operation + 1, CharArray.length).equals(RomanNumber[value2])) {
+            return Roman(Result(value1, value2));
+        } else if (s1.trim().replace(" ", "").substring(0, Operation).equals(ArabicNumber[value1]) && s1.trim().replace(" ", "").substring(Operation + 1, CharArray.length).equals(ArabicNumber[value2])) {
+            return Integer.toString(Result(value1, value2));
+        } else {
+            throw new RuntimeException("Некорректно введено выражение. Выражение должно содержать только арабские или только римские цифры от 1 до 10 и обязательно один знак: 1 + 1 или I + V");
         }
-        return s1;
+
+
     }
 
     public static void main(String[] args) {
-        try {do {
-            System.out.println("Итог выражения : " + StringToNumber());
-        }while (true);
-        }catch (Exception e){
-            System.out.println("Некорректно введено выражение. Выражение должно содержать только арабские или только римские цифры от 1 до 10 и обязательно один знак: 1 + 1 или I + V");
-        }
+            try {
+                System.out.println(StringToNumber());
+            } catch (Exception e) {
+                System.out.println("Некорректно введено выражение. Выражение должно содержать только арабские или только римские цифры от 1 до 10 и обязательно один знак: 1 + 1 или I + V");
+            }
     }
-
 }
